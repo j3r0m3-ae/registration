6328cf7482180cac257cd5fad62791164251d1bf
Artur Rychenkov <arychenkov@market-soft.ru>
2019-09-17 13:39:39 +0300

    add routing

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
-echo "Start of Development";
+// TODO: remove these settings at the end of development
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+define('ROOT', __DIR__);
+
+require './vendor/autoload.php';
+
+$routes = require './app/config/routes.php';
+
+$controllersMapping = require './app/config/controllersMapping.php';
+$controllerFactory = new \app\components\ControllerFactory($controllersMapping);
+
+(new \app\components\Router($routes, $controllerFactory))->run();
